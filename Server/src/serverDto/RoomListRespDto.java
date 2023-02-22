036e3af06b47e8fdc1350cd0631933604c89da8f
0476a <0476a@naver.com>
2023-02-22 17:51:17 +0900

    몇차 수정이지...

---
+++ Server/src/serverDto/RoomListRespDto.java
@@ -0,0 +1,12 @@
+package serverDto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RoomListRespDto {
+	private List<String> connectedRooms;
+}
